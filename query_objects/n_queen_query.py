37108a176c2b94f8c1d9078f162201ae800bd31b
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2020-09-24 19:59:52 -0500

    fix psql issue

--- query_objects/n_queen_query.py
+++ query_objects/n_queen_query.py
@@ -4,4 +4,4 @@ class NQueenQuery():
 
   @classmethod
   def find_board(cls, board_size):
-    return NQueen.query.filter_by(board_size=board_size).first()
+    return NQueen.query.filter_by(board_size=str(board_size)).first()
